ca41df4e705ba0d6083e12594c5c9394309fa02a
Paige Niedringhaus <paige_e_niedringhaus@homedepot.com>
2018-10-05 15:07:05 -0400

    toggling and heading for render props

---
+++ src/Toggle.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+export default class Toggle extends Component {
+  state = {
+    on: false,
+  };
+
+  toggle = () => {
+    this.setState({
+      on: !this.state.on,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        {this.state.on && this.props.children}
+        <button onClick={this.toggle}>Show/Hide</button>
+      </div>
+    );
+  }
+}
