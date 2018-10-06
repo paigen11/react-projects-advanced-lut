65f8d7352a34af0c01d13595700dd1194b54e598
Paige Niedringhaus <paige_e_niedringhaus@homedepot.com>
2018-10-06 15:54:06 -0400

    children render props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Toggle from './ToggleRenderProps';
+import Toggle from './ToggleRPC';
 
 class App extends Component {
   render() {
@@ -11,22 +11,14 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <Toggle
-          render={({ on, toggle }) => (
+        <Toggle>
+          {({ on, toggle }) => (
             <div>
               {on && <h1>Show me</h1>}
               <button onClick={toggle}>Show / Hide</button>
             </div>
           )}
-        />
-        <Toggle
-          render={({ on, toggle }) => (
-            <div>
-              {on && <nav>nav</nav>}
-              <button onClick={toggle}>Menu</button>
-            </div>
-          )}
-        />
+        </Toggle>
       </div>
     );
   }
