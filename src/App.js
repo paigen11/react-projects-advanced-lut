4822e416a4c1d4eae405147b1988372661913847
Paige Niedringhaus <paige_e_niedringhaus@homedepot.com>
2018-10-08 11:12:29 -0400

    portals!

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Toggle from './ToggleRPC';
+import Portal from './Portal';
 
 class App extends Component {
   render() {
@@ -16,6 +17,7 @@ class App extends Component {
             <>
               {on && <h1>Show me</h1>}
               <button onClick={toggle}>Show / Hide</button>
+              <Portal>{on && <h1>Hi, I'm in a portal!</h1>}</Portal>
             </>
           )}
         </Toggle>
--- src/Portal.js
+++ src/Portal.js
@@ -3,11 +3,26 @@ import ReactDOM from 'react-dom';
 
 // since portal is at the root of the project in the index.html file,
 // use plain JS to target it for use with React
-const modalRoot = document.getElementById('portal');
+const portalRoot = document.getElementById('portal');
 
 export default class Portal extends Component {
+  constructor() {
+    super();
+    this.el = document.createElement('div');
+  }
+
+  // add the new div to the portal root element
+  componentDidMount = () => {
+    portalRoot.appendChild(this.el);
+  };
+
+  // remove the div from the portal root whenever it unmounts
+  componentWillUnmount = () => {
+    portalRoot.removeChild(this.el);
+  };
+
   render() {
     const { children } = this.props;
-    return ReactDOM.createPortal(children);
+    return ReactDOM.createPortal(children, this.el);
   }
 }
