3f7d0b4050717ccb28eea15f0a619ad7b4329b87
Paige Niedringhaus <paige_e_niedringhaus@homedepot.com>
2018-10-15 18:56:08 -0400

    modals ftw

--- src/Modal.js
+++ src/Modal.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import Portal from './Portal';
+import Icon from './Utilities/Icon';
 
 export default class Modal extends Component {
   render() {
@@ -9,8 +10,13 @@ export default class Modal extends Component {
       <Portal>
         {on && (
           <ModalWrapper>
-            <button onClick={toggle}>Close</button>
-            <div>{children}</div>
+            <ModalCard>
+              <CloseButton onClick={toggle}>
+                <Icon name="close" />
+              </CloseButton>
+              <div>{children}</div>
+            </ModalCard>
+            <Background onClick={toggle} />
           </ModalWrapper>
         )}
       </Portal>
@@ -24,5 +30,37 @@ const ModalWrapper = styled.div`
   left: 0;
   width: 100%;
   height: 100%;
-  background: teal;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ModalCard = styled.div`
+  position: relative;
+  background: white;
+  border-radius: 5px;
+  box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.3);
+  padding: 15px;
+  z-index: 10;
+  min-width: 320px;
+  margin-bottom: 100px;
+`;
+
+const CloseButton = styled.button`
+  position: absolute;
+  top: 0;
+  right: 0;
+  border: none;
+  background: transparent;
+  padding: 10px;
+`;
+
+const Background = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  background: black;
+  opacity: 0.4;
 `;
