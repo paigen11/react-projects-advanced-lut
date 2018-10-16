ec3ba56601f19924b66d8b42d240f4d5010da144
Paige Niedringhaus <paige_e_niedringhaus@homedepot.com>
2018-10-16 17:03:57 -0400

    context

--- src/App.js
+++ src/App.js
@@ -1,28 +1,50 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component, Fragment, createContext } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Toggle } from 'Utilities';
 import { Modal } from 'Elements';
 
+const UserContext = createContext();
+
+class UserProvider extends Component {
+  state = {
+    id: '123',
+    name: 'Paige',
+    email: 'paige@gmail.com',
+  };
+
+  return() {
+    <UserContext.Provider
+      value={{
+        user: this.state,
+      }}
+    >
+      {this.props.children}
+    </UserContext.Provider>;
+  }
+}
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <Toggle>
-          {({ on, toggle }) => (
-            <>
-              <button onClick={toggle}>Login</button>
-              <Modal on={on} toggle={toggle}>
-                <h1>Once again, still in modal</h1>
-              </Modal>
-            </>
-          )}
-        </Toggle>
-      </div>
+      <UserProvider>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <Toggle>
+            {({ on, toggle }) => (
+              <>
+                <button onClick={toggle}>Login</button>
+                <Modal on={on} toggle={toggle}>
+                  <h1>Once again, still in modal</h1>
+                </Modal>
+              </>
+            )}
+          </Toggle>
+        </div>
+      </UserProvider>
     );
   }
 }
