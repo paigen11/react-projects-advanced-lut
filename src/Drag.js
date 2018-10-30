bfc35f6ddcddc714a08e7b486ea61bf2f4e40d16
Paige Niedringhaus <paige_e_niedringhaus@homedepot.com>
2018-10-30 17:03:36 -0400

    more rotation

--- src/Drag.js
+++ src/Drag.js
@@ -1,7 +1,23 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import { Gesture } from 'react-with-gesture';
 import { Spring, animated, interpolate } from 'react-spring';
 import { Card } from 'Elements';
+
+const AnimCard = Card.withComponent(animated.div);
+
+const DragCard = AnimCard.extend`
+  height: 300px;
+  position: absolute;
+`;
+
+const CardContainer = styled.div`
+  position: relative;
+  background: #ccc;
+  max-width: 320px;
+  margin: 0 auto;
+  border-radius: 5px;
+`;
 
 export default class Drag extends Component {
   render() {
@@ -9,19 +25,25 @@ export default class Drag extends Component {
       <Gesture>
         {({ down, xDelta }) => (
           <Spring
+            native
             to={{
               x: down ? xDelta : 0,
             }}
             immediate={name => down && name === 'x'}
           >
             {({ x }) => (
-              <Card
-                style={{
-                  transform: `translateX(${x}px)`,
-                }}
-              >
-                <h1>Drag me </h1>
-              </Card>
+              <CardContainer>
+                <DragCard
+                  style={{
+                    transform: interpolate(
+                      [x, x],
+                      (x, rotate) => `translateX(${x}px) rotate(${rotate} deg)`,
+                    ),
+                  }}
+                >
+                  <h1>Drag me </h1>
+                </DragCard>
+              </CardContainer>
             )}
           </Spring>
         )}
