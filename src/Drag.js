8dff0676c99af6cc63cef1da6ac261b3cae6d8dc
Paige Niedringhaus <paige_e_niedringhaus@homedepot.com>
2018-11-15 17:07:58 -0500

    sick animations with react-spring

--- src/Drag.js
+++ src/Drag.js
@@ -9,9 +9,10 @@ const AnimCard = Card.withComponent(animated.div);
 const DragCard = AnimCard.extend`
   height: 300px;
   position: absolute;
+  width: 320px;
 `;
 
-const CardContainer = styled.div`
+const CardContainer = styled(animated.div)`
   position: relative;
   background: #ccc;
   max-width: 320px;
@@ -20,6 +21,15 @@ const CardContainer = styled.div`
 `;
 
 export default class Drag extends Component {
+  onUp = xDelta => () => {
+    console.log(xDelta);
+    if (xDelta < -300) {
+      alert('Remove Card');
+    } else if (xDelta > 300) {
+      alert('Accept Card');
+    }
+  };
+
   render() {
     return (
       <Gesture>
@@ -32,12 +42,33 @@ export default class Drag extends Component {
             immediate={name => down && name === 'x'}
           >
             {({ x }) => (
-              <CardContainer>
+              <CardContainer
+                style={{
+                  background: x.interpolate({
+                    range: [-300, 300],
+                    output: [`#FF1C68`, `#14D790`],
+                    extrapolate: 'clamp',
+                  }),
+                }}
+              >
                 <DragCard
+                  onMouseUp={this.onUp(xDelta)}
                   style={{
+                    opacity: x.interpolate({
+                      range: [-300, -100],
+                      output: [0, 1],
+                      extrapolate: 'clamp',
+                    }),
                     transform: interpolate(
-                      [x, x],
-                      (x, rotate) => `translateX(${x}px) rotate(${rotate} deg)`,
+                      [
+                        x,
+                        x.interpolate({
+                          range: [-300, 300],
+                          output: [-45, 45],
+                          extrapolate: 'clamp',
+                        }),
+                      ],
+                      (x, rotate) => `translateX(${x}px) rotate(${rotate}deg)`,
                     ),
                   }}
                 >
